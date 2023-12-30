c84b060b68a0488bbbd0c80886c546877b69638d
aptheparker <apapap1119@g.skku.edu>
2023-12-31 00:24:03 +0900

    feat: sqlite -> mysql

--- prisma/migrations/20231225132406_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Reservation" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
-    "time" DATETIME NOT NULL,
-    "description" TEXT
-);
